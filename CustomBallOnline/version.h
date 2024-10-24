74a9e2e7772b4bdc335156973a5172af7e50d565
smallest-cock <soccerstarslegend420@gmail.com>
2024-10-23 17:09:19 -0700

    update sdk

--- CustomBallOnline/version.h
+++ CustomBallOnline/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 0
 #define VERSION_PATCH 6
-#define VERSION_BUILD 1
+#define VERSION_BUILD 2
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
