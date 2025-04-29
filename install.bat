497e46be10a3a0a522877b5b537293b3bccf63f3
smallest-cock <soccerstarslegend420@gmail.com>
2025-04-28 19:07:38 -0700

    switch to submodules

--- install.bat
+++ install.bat
@@ -56,37 +56,6 @@ IF %ERRORLEVEL% EQU 0 (
 )
 
 
-rem -------------------------- download plugin assets --------------------------
-
-rem Define the folder for assets
-set "ASSET_DIR=%APPDATA%\bakkesmod\bakkesmod\data\sslow_plugin_assets"
-
-rem Create the folder if it doesn't exist
-if not exist "%ASSET_DIR%" (
-    mkdir "%ASSET_DIR%"
-    echo Created folder: %ASSET_DIR%
-)
-
-rem Define image URLs and local file paths
-set "URL1=https://raw.githubusercontent.com/smallest-cock/plugin-assets/main/images/settings-footer/discord.png"
-set "URL2=https://raw.githubusercontent.com/smallest-cock/plugin-assets/main/images/settings-footer/github.png"
-set "URL3=https://raw.githubusercontent.com/smallest-cock/plugin-assets/main/images/settings-footer/youtube.png"
-
-set "FILE1=%ASSET_DIR%\discord.png"
-set "FILE2=%ASSET_DIR%\github.png"
-set "FILE3=%ASSET_DIR%\youtube.png"
-
-rem Download the images
-echo.
-echo.
-echo Downloading assets...
-echo.
-echo.
-
-curl -o "%FILE1%" "%URL1%" || echo Failed to download discord.png
-curl -o "%FILE2%" "%URL2%" || echo Failed to download github.png
-curl -o "%FILE3%" "%URL3%" || echo Failed to download youtube.png
-
 
 
 rem -------------------------- success message --------------------------
