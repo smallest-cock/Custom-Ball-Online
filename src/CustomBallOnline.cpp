ee3dccd2d7750f3b07d3965bd7b15c02f7dcb15b
smallest-cock <soccerstarslegend420@gmail.com>
2025-10-02 17:16:38 -0700

    update globals init function

--- src/CustomBallOnline.cpp
+++ src/CustomBallOnline.cpp
@@ -11,9 +11,7 @@ void CustomBallOnline::onLoad()
 {
 	_globalCvarManager = cvarManager;
 
-	// init globals
-	Instances.InitGlobals();
-	if (!Instances.CheckGlobals())
+	if (!Instances.InitGlobals())
 		return;
 
 	Format::construct_label({41, 11, 20, 6, 8, 13, 52, 12, 0, 3, 4, 52, 1, 24, 52, 44, 44, 37, 14, 22}, h_label);
